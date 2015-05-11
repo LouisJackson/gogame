a838f70dcb7da40d43e1a0a67ff468bb21a3d62e
LouisJackson <louis.amiot92@gmail.com>
2015-05-11 22:21:45 +0200

    Opponent recognition

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ function GoGame (size) {
 	this.size = size; //the size of the board
 	this.board; //we create a new empty board
 	this.currentPlayer = 'black';
+	this.oppositePlayer;
 	this.create = function() {
 		var m = new Array();
 		for (var i = 0; i < this.size; i++) {
@@ -32,11 +33,52 @@ function GoGame (size) {
 			var td = $('td[dataX="'+x+'"][dataY="'+y+'"]');
 			if(!td.hasClass('black') && !td.hasClass('white')) {
 				td.removeClass().addClass(this.currentPlayer);
+				this.board[x][y] = this.currentPlayer;
+				this.checkNeighbors(x,y);
 				this.switchPlayers();
 			}
 
 	}
-	//method that switch color to black
+	//method that switch color to black or white depends on the current player
+
+	this.checkNeighbors = function(x,y) {
+		if (this.currentPlayer == 'black') {
+			this.oppositePlayer = 'white';
+		}
+		else {
+			this.oppositePlayer = 'black';
+		}
+		x = parseInt(x);
+		y = parseInt(y);
+		var opponent = false;
+		console.log(x+' '+y);
+		if (x-1 >= 0) {
+			if (this.board[x-1][y] == this.oppositePlayer) {
+				opponent = x-1+' '+y;
+			}
+		}
+		console.log(x+1);
+		console.log(this.size);
+		console.log(this.oppositePlayer);
+		console.log(this.board[x+1][y]);
+		if (x+1 < this.size) {
+			if (this.board[x+1][y] == this.oppositePlayer) {
+				opponent = x+1+' '+y;
+			}
+		}
+
+		if (this.board[x][y-1] == this.oppositePlayer) {
+				opponent = x+' '+y-1;
+		}
+
+		if (this.board[x+1][y+1] == this.oppositePlayer) {
+				opponent = x+' '+y+1;
+		}
+
+		if (opponent) {
+			console.log('There is an opponent which position is'+ opponent);
+		}
+	}
 
 	this.switchPlayers = function() {
 		if (this.currentPlayer == 'black') {
@@ -57,7 +99,6 @@ game.render(board); //we display the board
 $('td').on('click',function(){
 	var x = $(this).attr('dataX');
 	var y = $(this).attr('dataY');
-	console.log(x + ' ' + y);
 	game.switchColor(x,y); // ... by calling the method of the object
 	
 });
