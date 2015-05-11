46ea853203f9ab05fa37d87d4941f44bf140b93e
LouisJackson <louis.amiot92@gmail.com>
2015-05-11 16:13:25 +0200

    Add switching color

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,33 @@
 function GoGame (size) {
 	this.size = size;
+	this.board;
 	this.create = function() {
 		var m = new Array();
 		for (var i = 0; i < this.size; i++) {
         	m[i] = new Array();
         	for (var j = 0; j < this.size; j++)
-            	m[i][j] = 'black';
+            	m[i][j] = false;
     	}
+    	this.board = m;
     	return m;
 	};
+
 	this.render = function(board) {
 		$('#table').append('<table><tbody>');
 		for (var i = 0; i < board.length; i++) {
 			$('#table').append('<tr>');
 			for(var j=0; j< board.length; j++) {
-				if (board[i][j] == 'black') {
-					$('#table').append('<td class="black">'+board[i][j]+'</td>');	
-				}
-				else {
-					$('#table').append('<td>'+board[i][j]+'</td>');
-				}
+				$('#table').append('<td dataX="'+i+'" dataY="'+j+'">'+board[i][j]+'</td>');
 			}
 			$('#table').append('</tr>');
-		};
+		}
 		$('#table').append('</tbody></table>');
+		};
+		
+	this.switchColor = function(color,x,y) {
+		if (color == 'black') {
+			$('td[dataX="'+x+'"][dataY="'+y+'"]').removeClass().addClass('black');
+		}
 	}
 }
 
@@ -31,6 +35,3 @@ var game = new GoGame(4);
 
 var board = game.create();
 game.render(board);
-
-
-console.log(board);
